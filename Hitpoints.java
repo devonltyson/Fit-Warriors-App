b713bd6b423082807acb5c6c761c7c5a2c59cabf
Devon Tyson <devonltyson@txstate.edu>
2019-09-21 01:41:58 -0500

    adding files

---
+++ Hitpoints.java
@@ -0,0 +1,16 @@
+
+public class Hitpoints extends Skill {
+	
+	int level, xp, goalXp;
+
+	public Hitpoints() {
+		this.level = 10;
+		this.xp = super.xp;
+		this.goalXp = super.goalXp;	
+	}
+	
+	@Override
+	public void train(int addedXp) {
+		this.xp += (addedXp/3); // hp levels slower than other skills
+	}
+}
